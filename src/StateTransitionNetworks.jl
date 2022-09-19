8314d8c06dbef044bf22488ee33687b9a81b06a8
Denes Karoly <kdenes@akceso.ch>
2022-09-19 14:30:23 +0300

    Analytic calculation of Sinai-Kolmogorov entropy added

--- src/StateTransitionNetworks.jl
+++ src/StateTransitionNetworks.jl
@@ -1,6 +1,6 @@
 module StateTransitionNetworks
 
-import Graphs: add_edge!, outneighbors, nv
+import Graphs: add_edge!, outneighbors, nv, weights
 import SimpleWeightedGraphs: SimpleWeightedDiGraph, get_weight
 import StatsBase: sample, mean, var, Weights
 
@@ -8,7 +8,7 @@ include("create_STN.jl")
 include("network_measures.jl")
 
 export timeseries_to_grid, create_STN
-export random_walk_on_weighted_graph, walk_statistics
+export random_walk_on_weighted_graph, walk_statistics, sinai_kolmogorov_entropy
 
 
 end
--- src/network_measures.jl
+++ src/network_measures.jl
@@ -37,3 +37,23 @@ function walk_statistics(ensemble, graph, N_steps, transient=5000)
     lyapunov_measure = var(walk_length)/(N_steps-transient)
     return entropy, lyapunov_measure
 end
+
+"""
+Calculates analytically the Sinai-Kolmogorov entropy on a STN. The main input `graph_P` is a SimpleWeightedDiGraph
+object containg the occurence probability of each transition (Q_ij). The additional `graph_P` argument is another 
+graph containg the normalized transition probabilities (P_ij). If this is not provided the function calculates automatically these transition
+probabilities.
+"""
+function sinai_kolmogorov_entropy(graph_Q; graph_P=nothing)
+    Q = Matrix(weights(graph_Q))
+    if isnothing(graph_P)
+        P = Matrix(weights(graph_Q))
+        for i in 1:size(Q)[1]
+            P[i,:]./=sum(Q[i,:])
+        end
+    else
+        P = Matrix(weights(graph_P))
+    end
+    entropy = -sum(Q[Q .!=0] .* log.(P[P .!=0]))
+    return entropy
+end
