9b833dfd27c2c7d5d8681971e5f6ab2b6a147421
rusandris <rusandris@gmail.com>
2022-10-11 16:58:08 +0300

    added plotting scripts

---
+++ tests/plot_convergence_henon.jl
@@ -0,0 +1,27 @@
+using DynamicalSystems
+using StateTransitionNetworks
+using Plots
+using LaTeXStrings
+
+ds = Systems.henon()
+params = [1.2,1.2265,1.24,1.27]
+ensemble = 1000
+N_max = 5000
+T = 10000
+Ttr = 1000
+steps = 1:N_max
+colors = [:orange,:red,:blue,:green]
+
+pl = plot()
+
+for (i,a) in enumerate(params)
+	set_parameter!(ds,1,a)
+	@show a
+	traj = trajectory(ds,T;Ttr = Ttr)
+	traj_grid, vertex_names = timeseries_to_grid(traj,20)
+	stn_q, stn_p = create_STN(traj_grid,vertex_names)
+	entr,lyaps = measure_convergence(stn_p,ensemble,N_max)
+	plot!(steps,lyaps,lw=2,lc = colors[i],xlabel="t",ylabel=L"$\Delta L/t$",xguidefontsize=18,yguidefontsize=18,tickfontsize=10,label="$a",legendfontsize=15,legend=:best,dpi=300)
+end
+
+savefig(pl,"henon_convergence_ens$ensemble"*"_N$N_max"*"_T$T"*"_Ttr$Ttr")
---
+++ tests/plot_convergence_lorenz.jl
@@ -0,0 +1,34 @@
+using DynamicalSystems
+using StateTransitionNetworks
+using Plots
+using LaTeXStrings
+using OrdinaryDiffEq	
+
+ds = Systems.lorenz()
+tol = 1e-8
+params = [180.7,180.1,180.78,181.1]
+ensemble = 1000
+N_max = 5000
+plane = (1,15.0)
+T = 5000
+Ttr = 500
+steps = 1:N_max
+colors = [:red,:orange,:green,:blue]
+grid_size = 20
+#diffeq = (alg = Tsit5(),dt = 1e-3,adaptive=true)
+
+
+pl = plot()
+
+for (i,ρ) in enumerate(params)
+	set_parameter!(ds,2,ρ)
+	@show ρ
+	psection = poincaresos(ds, plane, T; Ttr=Ttr, direction=+1,rootkw = (xrtol = tol, atol = tol));
+	traj = psection[:,2:end] 
+	traj_grid, vertex_names = timeseries_to_grid(traj,grid_size) 
+	stn_q, stn_p = create_STN(traj_grid,vertex_names)
+	entr,lyaps = measure_convergence(stn_p,ensemble,N_max)
+	plot!(steps,lyaps,lw=2,lc = colors[i],xlabel="t",ylabel=L"$\Delta L/t$",xguidefontsize=18,yguidefontsize=18,tickfontsize=10,label="$ρ",legendfontsize=15,legend=:best,dpi=300)
+end
+
+savefig(pl,"lorenz_convergence_ens$ensemble"*"_N$N_max"*"_T$T"*"_Ttr$Ttr")
