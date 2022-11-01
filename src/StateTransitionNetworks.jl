135febe7bfb3b9c9ba45ca0cb01de7792a2d1077
rusandris <rusandris@gmail.com>
2022-11-01 17:40:59 +0200

    added read_bin and methods to stn_analysis

--- src/StateTransitionNetworks.jl
+++ src/StateTransitionNetworks.jl
@@ -6,7 +6,7 @@ import StatsBase: sample, mean, var, Weights
 using MetaGraphsNext
 using GraphPlot,Cairo,Compose
 import SparseArrays: spzeros 
-import DelayEmbeddings: AbstractDataset
+import DelayEmbeddings: Dataset,AbstractDataset
 import ChaosTools: poincaresos
 
 include("create_stn.jl")
@@ -17,6 +17,6 @@ include("timeseries_analysis.jl")
 export timeseries_to_grid, create_stn,prob_matrix,weight_matrix
 export network_measures, sinai_kolmogorov_entropy,measure_convergence
 export plot_stn
-export stn_analysis
+export stn_analysis,read_bin
 
 end
--- src/timeseries_analysis.jl
+++ src/timeseries_analysis.jl
@@ -8,12 +8,38 @@ Calculates the Sinai-Kolmogorov Entropy and Lyapunov measure of a STN created fr
 * `ensemble` : number of individual random walks on the STN
 * `N_steps` : number of steps in each random walk
 """
-function stn_analysis(timeseries::AbstractDataset;grid,plane,ensemble=100,N_steps=10000)
+function stn_analysis(timeseries::Dataset;grid,plane,ensemble=100,N_steps=1000,return_stn=false)
 	dim = size(timeseries)[2]
 	psection = poincaresos(timeseries, plane); 
 	psection = psection[:,filter(x -> x != plane[1],1:dim)]
 	
 	discrete_timeseries, vertex_names = timeseries_to_grid(psection,grid)
-	stn = create_stn(discrete_timeseries,vertex_names)
-	network_measures(stn,ensemble,N_steps)
+	stn,retcode = create_stn(discrete_timeseries,vertex_names)
+	
+	if return_stn
+		return stn,retcode
+	end
+	
+	if retcode ==:Success
+		return network_measures(stn,ensemble,N_steps)
+	else
+		return NaN,NaN
+	end
 end
+
+stn_analysis(timeseries::Matrix;grid,plane,ensemble=100,N_steps=1000,return_stn=false) = stn_analysis(Dataset(timeseries);grid=grid,plane=plane,ensemble=ensemble,N_steps=N_steps,return_stn=return_stn)
+
+"""
+	read_bin(filename::String,T::DataType,dim) -> Matrix
+Reads data from `filename` with elements of type `T` and shape `(:,dim)`. 
+
+"""
+function read_bin(filename::String,T::DataType,dim)
+	data = reinterpret(T,read(filename))
+	return Matrix(reshape(data,(dim,:))')
+end
+
+
+
+
+
