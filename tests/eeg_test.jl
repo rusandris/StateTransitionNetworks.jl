f5320d507ce89a0fdcf5275e6e7af91490718595
rusandris <rusandris@gmail.com>
2022-11-01 17:44:39 +0200

    eeg test

---
+++ tests/eeg_test.jl
@@ -0,0 +1,19 @@
+using StateTransitionNetworks
+
+grid = 20
+plane = (1,0)
+ens = 1000
+steps = 10000
+filename = "sleep/BB0008_SP2_stg2_F3C3P3O1LOCA2EMG_7856640_7987712.dat"
+
+eeg_data = read_bin(filename,Float32,6)
+stn,retcode = stn_analysis(eeg_data;grid=grid,plane=plane,return_stn=true)
+
+@show retcode
+
+lyap, entr = network_measures(stn,ens,steps)
+@show lyap, entr
+
+fn, ext = splitext(filename)
+plot_stn(stn;filename=fn*".pdf",linetype="curve")
+
