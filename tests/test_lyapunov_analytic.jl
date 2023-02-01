77eb08c1dffbeec3fbaee18084e95f27b85a0214
sbulcsu <sbulcsu@gmail.com>
2023-02-01 16:47:21 +0200

    analytic lyapunov fixed

--- tests/test_lyapunov_analytic.jl
+++ tests/test_lyapunov_analytic.jl
@@ -8,17 +8,23 @@ using Statistics
 
 using LinearAlgebra
 
+function uniquetol(A; kws...)
+   S = []
+   for a in A
+        if !any(s -> isapprox(s, a; kws...), S)
+            push!(S, a)
+        end
+   end
+   return S
+end
+
 # Lyapunov measure for a given ρ value
 Δt = 0.001;
 plane = (1,15.0);
 grid = 20;
-ρ= 181.02; #180.9;
-u0 = rand(Float64,3).*50 .-25;
-# ρ = 180.91
-# u0 = [19.208757974895896, 8.16404896961214, 22.003233673468387]
-# # u0 = [-6.415632455803806, 22.288031139971572, -0.19849359973168035]
-# # u0 = [-3.7172591949410076, 19.267269973658813, -10.673236379143875]
-T = 3000;
+ρ = 180.9;
+u0 = rand(3)
+T = 5000;
 system = Systems.lorenz(u0; ρ=ρ);
 timeseries = trajectory(system, T; Δt=Δt, Ttr=500);
 
@@ -26,22 +32,32 @@ psection = ChaosTools.poincaresos(timeseries, plane; direction=+1, idxs=[2,3]);
 d_traj, v_names = timeseries_to_grid(psection, grid);
 stn, ret_code = create_stn(d_traj, v_names);
 ret_code
+#plot_stn(stn)
 
 P = prob_matrix(stn);
 Q = weight_matrix(stn);
 
-function analytic_lyapunov(P,Q)
+# OLD function for the analytic Lyapunov===============================================================
+
+function analytic_lyapunov_old(P,Q)
    λ, v = eigen(Matrix(P))
+   
+   if length(uniquetol(λ))<size(P)[1]
+      @show λ
+   end
+   
    @show det(v)
    if isapprox(det(v),0) 
       return -1, -1, -1, :SingularityError
    end
 
    λt, vt = eigen(transpose(Matrix(P)))
-
+    
+   
    if real(λ[end]) ≈ 1
       λ[end] = 1.0 + 0.0im
    end
+   
 
    if real(λt[end]) ≈ 1
       λt[end] = 1.0 + 0.0im
@@ -50,20 +66,26 @@ function analytic_lyapunov(P,Q)
 
    Ω = Diagonal(1 ./ (1 .- λ))
    replace!(Ω, NaN+NaN*im=>0.0)
-
+   
+   #=
+   for (i,λi) in enumerate(λ)
+      if abs(λi) ≈ 1
+         #@show λi
+         Ω[i,i] = 0.
+      end
+   end
+   =#
+   
    L = Matrix(-log.(P))
    replace!(L, Inf=>0.0)
+   L2 = P.*L.^2
    L = P.*L
 
-   v*inv(v)
-   det(v)
-   det(v)≈ 0.0+0.0im
    covariance = x*L*v*Ω*inv(v)*L*ones(length(λ))
-
-   avg = sinai_kolmogorov_entropy(Q,P)
-   sq_avg = sum(Q[Q .!=0] .* (log.(P[P .!=0])).^2)
-
-   variance = sq_avg - avg^2
+   #avg = sinai_kolmogorov_entropy(Q,P)
+   #sq_avg = sum(Q[Q .!=0] .* (log.(P[P .!=0])).^2)
+   #variance = sq_avg - avg^2
+   variance = x*L2*ones(length(λ))-(x*L*ones(length(λ)))^2
    lyapunov =  variance + 2*real(covariance)
    if imag(covariance) < 1.0e-3
       return lyapunov, variance, real(covariance), :Success
@@ -73,14 +95,46 @@ function analytic_lyapunov(P,Q)
    end
 end
 
-analytic_lyapunov(P,Q)
+# Analytic calculation of the Lyapunov measure ====================================================
+
+function analytic_lyapunov(P)
+   λ, V = eigen(Matrix(P))
+   λ, X = eigen(transpose(Matrix(P)))
+   
+   if real(λ[end]) ≈ 1
+      x = transpose(X[:,end]./sum(X[:,end]))
+      v = V[:,end]./V[1,end]
+   else
+      return -1, -1, -1, :StochasticMatrixError
+   end
+
+   L = Matrix(-log.(P))
+   replace!(L, Inf=>0.0)
+   L2 = P.*L.^2
+   L = P.*L
+   I = Diagonal(ones(length(x)))
+   S = (I-v*x)+(P-v*x)*inv(I-P+v*x)
+   covariance = x*L*S*L*v
+   variance = x*L2*v-(x*L*v)^2
+   lyapunov =  variance + 2*real(covariance)
+   if imag(covariance) < 1.0e-3
+      return lyapunov, variance, real(covariance), :Success
+   else
+      @show covariance
+      return lyapunov, variance, real(covariance), :ComplexCovariancveWarning
+   end
+end
+
+
+analytic_lyapunov_old(P,Q)
+analytic_lyapunov(P)
 network_measures(stn, 1000, 10^4)[2]
 
 
 # Analytic Lyapunov measure over an interval ======================================================================================
 
-rho = 180:0.005:182;
-T = 5000;
+rho = 180.:0.001:182.;
+T = 100000;
 ensemble = 1000;
 N_steps = 10000;
 data0 = [];
@@ -105,7 +159,7 @@ for ρ in rho
       P = prob_matrix(stn);
       Q = weight_matrix(stn);
 
-      lyapunov, variance, covariance, ret_code_lyap = analytic_lyapunov(P,Q)
+      lyapunov, variance, covariance, ret_code_lyap = analytic_lyapunov(P)
       if ret_code_lyap != :Success
          @show ret_code_lyap, lyapunov, u0
       end
@@ -124,6 +178,7 @@ plot!(rho, data0, label = "Analytic")
 plot!(rho, data1, label = "Variance")
 plot!(rho, data2, label = "Covariance")
 plot!(xlabel = "ρ", ylabel = "Λ", ylim=[-0.5,3.], xlim=[180.,182.], legend = :topleft)
+#plot!(xlabel = "ρ", ylabel = "Λ", ylim=[-0.5,3.], xlim=[180.85,181.], legend = :topleft)
 
 
 data[data .< 0]
