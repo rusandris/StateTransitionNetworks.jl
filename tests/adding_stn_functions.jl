d7987bb00597c858f2d6416d3df33ce6b4ab7b4d
rusandris <rusandris@gmail.com>
2023-05-12 14:51:51 +0300

    added pca data tests

---
+++ tests/adding_stn_functions.jl
@@ -0,0 +1,80 @@
+import StateTransitionNetworks.lyapunov_measure
+import SparseArrays: spzeros 
+import StateTransitionNetworks.renormalize
+import Graphs: DiGraph
+
+
+function timeseries_to_common_grid(timeseries, grid, x_min, x_max, y_min, y_max)    
+    T = length(timeseries[:,1])
+    dx = 0.5*(x_max-x_min)/grid
+    dy = 0.5*(y_max-y_min)/grid
+    x_grid = range(x_min-dx, x_max+dx, grid);
+    x_min = x_grid[1]
+    y_grid = range(y_min-dy, y_max+dy, grid);
+    y_min = y_grid[1]
+    x_n = Vector{Int64}(undef, T)
+    y_n = Vector{Int64}(undef, T)
+    x_n, y_n = [], [];
+
+    for row in eachrow(timeseries)
+        y = floor(Int,(row[2]-y_min)/Float64(y_grid.step))+1
+        x = floor(Int,(row[1]-x_min)/Float64(x_grid.step))+1
+        push!(x_n, x)
+        push!(y_n, y)
+    end
+    d_timeseries = [x_n y_n]
+    return d_timeseries
+end
+
+
+
+function add_timeseries(dt_list, grid;make_ergodic=false,verbose=false)
+    Q_null = zeros(Int32, grid*grid, grid*grid) # Null matrix with all possible transitions
+    vertex_names = [];  # Future name of vertices
+    vertex_place = [];  # Rows, and column number in Q_null for a given vertex
+    M = zeros(grid,grid)
+    nr_vertices = 0;
+    for dt in dt_list
+        states = []
+        O = zeros(Int32, grid*grid, grid*grid)
+        for row in eachrow(dt)
+            v = (row[1]-1)*grid + row[2]
+            x, y = row
+            if M[y,x] == 0
+                nr_vertices += 1 
+                push!(vertex_names, [nr_vertices, x, y])
+                push!(vertex_place, v)
+                M[y,x] = 1
+            end    
+            push!(states, v) 
+        end
+        #count transitions
+        next_states = circshift(states,-1)
+        for i in eachindex(states[1:end-1])
+            O[states[i],next_states[i]] += 1
+        end
+        Q_null = Q_null + O
+    end
+    vertex_names = reduce(hcat, vertex_names)'
+    Q = Q_null[vertex_place, vertex_place]
+	#weight and transition probability matrices
+    P = spzeros(nr_vertices, nr_vertices)
+	#normalize Q and fill P by normalizing rows
+    Q = Q./sum(Q)
+
+	P = renormalize(Q)
+	#create directed metagraph with static label and metadata types and default weight 0
+	stn, ret_code = create_stn(P;make_ergodic=make_ergodic,verbose=verbose)
+    for v in 1:nr_vertices
+		x,y = vertex_names[v,2:end] 
+		stn[v] = Dict(:x => x,:y => y)
+	end
+    return stn, ret_code
+end
+
+function get_grid_edges(psections)
+    x_max, y_max = maximum(reduce(vcat, maximum.(Matrix.(psections); dims=1)); dims=1)
+    x_min, y_min = minimum(reduce(vcat, minimum.(Matrix.(psections); dims=1)); dims=1)
+    return x_min, x_max, y_min, y_max
+end
+
---
+++ tests/test_pca.jl
@@ -0,0 +1,131 @@
+using StateTransitionNetworks
+using DelimitedFiles
+using StatsBase
+using DynamicalSystems
+import Graphs: nv,ne
+using Plots,LaTeXStrings
+
+include("adding_stn_functions.jl")
+
+function translate_to_origin!(epoch)
+	for i in 1:3
+		epoch[:,i] = epoch[:,i] .- mean(epoch[:,i])
+	end
+end
+
+
+function ndensity(stn)
+	nr_vertices = nv(stn)
+	ne(stn)/(nr_vertices*(nr_vertices-1))
+end
+
+
+#read data
+pca_data = readdlm("pca_data/sub-AH316_pca.txt")
+
+#-----------------params---------------------
+epoch_length = 386
+plane = (1,0.0)
+grid = 20
+
+
+#------------------get psos for every epoch------------------------------
+
+epoch_psections = []
+
+
+for epoch_start_index in 1:epoch_length:(size(pca_data)[1]-epoch_length)
+	epoch = pca_data[epoch_start_index:epoch_start_index+epoch_length-1,:]
+	#@show epoch_start_index
+	#@show size(epoch)[1]
+	
+	translate_to_origin!(epoch)
+	
+	psection = ChaosTools.poincaresos(Dataset(epoch), plane; direction=+1, idxs=[2,3]);
+	push!(epoch_psections,psection)
+	
+end
+
+#common grid extrema
+x_min, x_max, y_min, y_max = get_grid_edges(epoch_psections)
+
+#list of discrete timeseries for every epoch
+discrete_timeseries = timeseries_to_common_grid.(epoch_psections, grid, x_min, x_max, y_min, y_max);
+
+lyaps = []
+entropies = []
+densities = []
+nr_vertices = []
+
+
+#----------------------------create stns by adding epochs---------------------------------------
+
+for i in 1:length(discrete_timeseries)
+	stn_added,retcode = add_timeseries(discrete_timeseries[1:i],grid;make_ergodic=true,verbose=false)
+	@show nv(stn_added)
+	@show retcode
+	P = prob_matrix(stn_added)
+	if retcode == :Success
+		entropy,lyapunov = network_measures(P)
+		push!(entropies,entropy)
+		push!(lyaps,lyapunov)
+		push!(nr_vertices, nv(stn_added))
+		push!(densities,ndensity(stn_added))
+		
+		if i == length(discrete_timeseries)
+			plot_stn(stn_added;filename="stn_added.pdf",nodesize=0.6,nodefillc="orange",linetype="curve",max_edgelinewidth=1)
+		end
+		
+	end
+end
+
+
+#-----------------plotting-------------------
+
+pl = plot(lyaps,
+	tickfontsize=12,
+	dpi=300,
+	c=:red,
+	guidefontsize=15,
+	legendfontsize=12,
+	label=L"\Lambda",
+	xlabel=L"epochs ~ added",
+	ylabel=L"nv,~\rho,~S_{KS},~\Lambda",
+	framestyle=:box)
+
+
+plot!(entropies,
+	tickfontsize=12,
+	dpi=300,
+	c=:blue,
+	guidefontsize=15,
+	legendfontsize=12,
+	label=L"S_{KS}",
+	xlabel=L"epoch ~ added",
+	ylabel=L"nv,~\rho,~S_{KS},~\Lambda",
+	framestyle=:box)
+	
+plot!(densities,
+	tickfontsize=12,
+	dpi=300,
+	c=:orange,
+	guidefontsize=15,
+	legendfontsize=12,
+	label=L"\rho : density",
+	xlabel=L"epochs ~ added",
+	ylabel=L"nv,~\rho,~S_{KS},~\Lambda",
+	framestyle=:box)
+
+
+plot!(nr_vertices ./100,
+	tickfontsize=12,
+	dpi=300,
+	c=:green,
+	guidefontsize=15,
+	legendfontsize=12,
+	label=L"nr. vertices/100",
+	xlabel=L"epochs ~ added",
+	ylabel=L"nv,~\rho,~S_{KS},~\Lambda",
+	framestyle=:box)
+
+savefig(pl,"add_stn_epoch_test.pdf")
