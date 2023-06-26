813d1fd8b6b15ce859bdd6ef5449a884ebf684ef
rusandris <rusandris@gmail.com>
2023-06-26 07:18:09 +0300

    add adjustable weight exponent option

--- src/plot_stn.jl
+++ src/plot_stn.jl
@@ -1,4 +1,4 @@
-function plot_stn(stn;filename="stn.pdf",nodesize=1,nodefillc="orange",linetype="straight",max_edgelinewidth=1,nodelabels=false,kwargs...)
+function plot_stn(stn;filename="stn.pdf",nodesize=1,nodefillc="orange",linetype="straight",max_edgelinewidth=1,weight_exponent=1,nodelabels=false,kwargs...)
 	nr_vertices = nv(stn)
 	x = []
 	y = []
@@ -14,7 +14,7 @@ function plot_stn(stn;filename="stn.pdf",nodesize=1,nodefillc="orange",linetype=
 	w = weight_matrix(stn)
 	
 	gp = gplot(stn,x,-y;kwargs...,
-		edgelinewidth = reduce(vcat, [w[i,:].nzval for i in 1:nv(stn)]) .^ 0.33,
+		edgelinewidth = reduce(vcat, [w[i,:].nzval for i in 1:nv(stn)]) .^ weight_exponent,
 		nodelabel = if nodelabels 1:nv(stn) else nothing end,
 		nodesize=nodesize,
 		nodefillc=nodefillc,
