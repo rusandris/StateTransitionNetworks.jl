b5c90866b8a82eba12300a816bcc88d4c6c0b4f6
sbulcsu <sbulcsu@gmail.com>
2023-08-02 18:25:59 +0300

    logistic map tests

--- src/test_logistic_map.jl
+++ src/test_logistic_map.jl
@@ -41,11 +41,11 @@ end
 ### Measures for a single value
 ###############################
 
-grid_size = 200;
+grid_size = 20;
 r = 4.;
 ds = Systems.logistic(0.4; r = r)
 λ = lyapunov(ds, 10000; Ttr = 500)
-timeseries = trajectory(ds, 100000, 0.4; Ttr=1000);
+timeseries = trajectory(ds, 10^8, 0.4; Ttr=1000);
 ys = zeros(length(timeseries))
 timeseries = hcat(timeseries, ys)
 discrete_timeseries, vertex_names = timeseries_to_grid(timeseries, grid_size; grid_edges = [0., 0., 1., 1.]);
@@ -53,14 +53,15 @@ discrete_timeseries, vertex_names = timeseries_to_grid(timeseries, grid_size; gr
 P = prob_matrix(stn);
 @time S, Λ = network_measures(P)
 @time C1, C2 = bit_number_measures(P) 
+
 ####################
 ### network measures
 ####################
 
-r_values = 3.5:0.001:4.;
-traj_length = 100000;
+r_values = 3.995:0.001:4.;
+traj_length = 10^8;
 trans = 1000;
-grid_size = 1000;
+grid_size = 20;
 ensemble = 1000;
 N_steps = 10000;
 
@@ -82,20 +83,45 @@ for (i,r) in enumerate(r_values)
     #sim_entropy_measures[i], sim_lyapunov_measures[i] = network_measures(stn, ensemble, N_steps)
     P = prob_matrix(stn);
     theor_entropy_measures[i], theor_lyapunov_measures[i] = network_measures(P)
-    C1_measures[i], C2_measures[i] = bit_number_measures(P)
+    #C1_measures[i], C2_measures[i] = bit_number_measures(P)
 end
 
 data = hcat(r_values, sim_entropy_measures, sim_lyapunov_measures, theor_entropy_measures, theor_lyapunov_measures, C1_measures, C2_measures)
 
 #save data
 f_name = "./tests/logistic_measures_r=3.5-4_da=0.001_ens=1000_tmax=100000_ttrans=1000_grid=200.dat"
-f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=200.dat"
-f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=1000.dat"
 f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=20.dat"
+f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=10^2_ttrans=1000_grid=200.dat"
+f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=10^3_ttrans=1000_grid=200.dat"
+f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=10^4_ttrans=1000_grid=200.dat"
+f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=10^5_ttrans=1000_grid=200.dat"
+f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=200.dat"
+f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=10^7_ttrans=1000_grid=200.dat"
+f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=1000.dat"
+f_name = "./tests/logistic_measures_r=3.5-4_da=0.0001_tmax=1000000_ttrans=1000_grid=2000.dat"
 writedlm(f_name,data)
 # load data
 f_name = "./tests/logistic_measures_r=0-4_da=0.01_ens=1000_tmax=100000_ttrans=1000_grid=200.dat"
 data = readdlm(f_name)
+
+# plot bit number measures C2
+pl = plot()
+plot!(pl, data[:,1], data[:,5], label = L"\Lambda", lw=2, alpha=0.5, color="red")
+plot!(pl, data[:,1], data[:,7], label = L"C_2", lw=2, alpha=0.5, color="orange")
+plot!(pl, xlabel=L"r", ylabel=L"\Lambda, C_2", xguidefontsize=22, yguidefontsize=22, xlim=[3.5,4], tickfontsize=14, lw=2, fontfamily="Serif", legendfontsize=16,
+dpi=300)
+plot!(ylim=[0,1], xlim=[3.6,3.7])
+savefig(pl, "./tests/logistic_var_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=20.svg")
+savefig(pl, "./tests/logistic_var_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=20.pdf")
+
+# plot bit number measures C1
+pl = plot()
+plot!(pl, data[:,1], data[:,4], label = L"S", lw=2, alpha=0.5, color="black")
+plot!(pl, data[:,1], data[:,6], label = L"C_1", lw=2, alpha=0.5, color="brown")
+plot!(pl, xlabel=L"r", ylabel=L"S, C_1", xguidefontsize=22, yguidefontsize=22, xlim=[3.5,4], tickfontsize=14, lw=2, fontfamily="Serif", legendfontsize=16,
+dpi=300)
+savefig(pl, "./tests/logistic_entr_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=20.svg")
+savefig(pl, "./tests/logistic_entr_r=3.5-4_da=0.0001_tmax=100000_ttrans=1000_grid=20.pdf")
 
 pl = plot()
 #scatter!(pl, data[:,1], data[:,2], label = "Random walk", lw=2, color="gray", alpha=0.5)
