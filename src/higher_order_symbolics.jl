fa7366fa3250986956b782cf11aec2d4d982ff2c
sbulcsu <sbulcsu@gmail.com>
2024-02-20 18:05:04 +0200

    type stable higher order symbolics functions

--- src/higher_order_symbolics.jl
+++ src/higher_order_symbolics.jl
@@ -1,14 +1,14 @@
 export higher_order_symbolics!
 
-function higher_order_symbolics!(symbolic_timeseries, order; delays = collect(0:order-1))
+function higher_order_symbolics!(symbolic_timeseries::Vector{T}, order; delays = collect(0:order-1)) where T<:Integer
     length(delays) != order && @error "The number of provided delays has to be the same as `order`"
     delays[1] != 0 && @error "The first delay has to be zero, viz. the actual element."
     # rename symbols from 1 to nr_symbols
-    symbols = sort!(unique(symbolic_timeseries))
-	nr_symbols = length(symbols)
-    symbol_dictionary = Dict(symbols .=> 1:nr_symbols)
+    symbols::Vector{T} = sort!(unique(symbolic_timeseries))
+	nr_symbols::T = length(symbols)
+    symbol_dictionary = Dict(symbols .=> collect(UnitRange{T}(1, nr_symbols)))
     # higher order state in vector formalism
-    states = zeros(order)
+    states::Vector{T} = Vector{T}(undef, order)
     # new symbolic timeseries by OVERWRITING the old timeseries vector
     order>1 ? max_delay=maximum(delays) : max_delay=0
     for i in 1+max_delay:length(symbolic_timeseries)
@@ -19,11 +19,13 @@ function higher_order_symbolics!(symbolic_timeseries, order; delays = collect(0:
         # encode the higher order state
         symbolic_timeseries[i-max_delay] = higher_order_state(states, nr_symbols)
     end
+    #return @view symbolic_timeseries[1:length(symbolic_timeseries)-max_delay]
     return nothing
 end
 
-function higher_order_state(states, nr_symbols)
-    hs = 0
+
+function higher_order_state(states::Vector{T}, nr_symbols::T) where T<:Integer
+    hs::T = 0
     for (i,s) in enumerate(states)
         hs += s*nr_symbols^(i-1)
     end
