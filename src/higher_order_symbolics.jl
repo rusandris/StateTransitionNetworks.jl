2021d13eb9f884ebbdc6149b23aba1727098d576
rusandris <rusandris@gmail.com>
2024-05-14 16:30:20 +0300

    add overflow warning

--- src/higher_order_symbolics.jl
+++ src/higher_order_symbolics.jl
@@ -1,10 +1,11 @@
 export higher_order_symbolics!
 
 function higher_order_symbolics!(symbolic_timeseries::Vector{T}, order::Int; delays = collect(0:order-1)) where T<:Integer
-    length(delays) != order && @error "The number of provided delays has to be the same as `order`"
-    delays[1] != 0 && @error "The first delay has to be zero, viz. the actual element."
+    length(delays) != order && throw(ArgumentError("The number of provided delays has to be the same as `order`"))
+    delays[1] != 0 && throw(ArgumentError("The first delay has to be zero, viz. the actual element."))
+    warn=false
+    nr_symbols = maximum(symbolic_timeseries) #assumed symbols go from 1 to nr_symbols
 
-    nr_symbols = maximum(symbolic_timeseries)
     # new symbolic timeseries by OVERWRITING the old timeseries vector
     order>1 ? max_delay=maximum(delays) : max_delay=0
     for i in 1+max_delay:length(symbolic_timeseries)
@@ -14,9 +15,12 @@ function higher_order_symbolics!(symbolic_timeseries::Vector{T}, order::Int; del
             s = symbolic_timeseries[i-delays[d]]
             hs += s*nr_symbols^(d-1)
         end
+        #warn if overflow happens
+        hs < 0 && (warn = true) 
         # encode the higher order state
         symbolic_timeseries[i-max_delay] = hs
     end
+    warn && @warn "Overflowing integer symbols when calculating higher order states!"
     return nothing
 end
 
